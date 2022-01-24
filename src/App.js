0d5d847a1ca08679d58a8dc69d83eda58f0408f5
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2022-01-25 00:12:57 +0500

    :white_check_mark: update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import { useState } from 'react';
-import './App.css';
-import Card from './components/Card';
+import { useState } from "react";
+import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   const [images, setImages] = useState([
-    { url: "https://source.unsplash.com/random/1"},
-    { url: "https://source.unsplash.com/random/2"},
-    { url: "https://source.unsplash.com/random/3"},
-    { url: "https://source.unsplash.com/random/4"},
-    { url: "https://source.unsplash.com/random/5"},
-    { url: "https://source.unsplash.com/random/6"},
+    { url: "https://source.unsplash.com/random/1" },
+    { url: "https://source.unsplash.com/random/2" },
+    { url: "https://source.unsplash.com/random/3" },
+    { url: "https://source.unsplash.com/random/4" },
+    { url: "https://source.unsplash.com/random/5" },
+    { url: "https://source.unsplash.com/random/6" },
   ]);
 
   const fetchNewImage = () => {
-    setImages([...images, {
-      url : `https://source.unsplash.com/random/${Math.floor(Math.random() * 100)}`
-    }])
-  }
+    const response = `https://source.unsplash.com/random/${Math.floor(
+      Math.random() * 100
+    )}`;
+    setImages([
+      ...images,
+      {
+        url: response,
+      },
+    ]);
+  };
 
   const removeImage = () => {
-    setImages(images.slice(0, -1))
-  }
+    setImages(images.slice(0, -1));
+  };
 
   return (
     <section className="hero">
